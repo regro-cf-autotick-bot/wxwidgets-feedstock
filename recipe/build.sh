3d37aabba2527dd5fbe0a49306614c4a24d925a3
Mark Harfouche <mark.harfouche@gmail.com>
2020-10-24 07:54:46 -0400

    Reorder OSX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,18 @@
-# Use Gold linker and -Wl,--as-needed as suggested in
-# https://github.com/conda-forge/staged-recipes/pull/11589#issuecomment-629882668
-# export LDFLAGS="${LDFLAGS:-} -fuse-ld=gold -Wl,--as-needed"
+#!/usr/env/bin bash
+
+set -ex
+echo $target_platform
+
+if [[ "$target_platform" == "osx"* ]]; then
+  TARGET_PLATFORM_CONFIGURE_FLAGS="--with-osx_cocoa"
+else
+  TARGET_PLATFORM_CONFIGURE_FLAGS="--with-gtk=\"3\""
+fi
 
 ./configure \
   --prefix=${PREFIX} \
-  --with-gtk="3" \
-  --with-opengl
+  --with-opengl \
+  ${TARGET_PLATFORM_CONFIGURE_FLAGS} || cat config.log
 
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 
