d97b98aa2a1cac4da2c7432882cf66e815bc89b7
Mark Harfouche <mark.harfouche@gmail.com>
2021-04-14 15:40:51 -0400

    Try to specify custom vedor

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,9 @@ if errorlevel 1 exit 1
 cd build_
 if errorlevel 1 exit 1
 
+:REM The very first version of the library we built had the dlls ending in
+:REM x64_custom. We maintain it here for backward compatibility
+
 cmake  ^
     -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -12,6 +15,7 @@ cmake  ^
     -DwxUSE_LIBJPEG=sys ^
     -DwxUSE_LIBPNG=sys  ^
     -DwxUSE_LIBTIFF=sys ^
+    -DwxVENDOR=x64_custom ^
     ..
 if errorlevel 1 exit 1
 
